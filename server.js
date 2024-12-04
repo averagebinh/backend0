2fcb47058f842f897a8fb4125c26d6763207c667
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-12-04 16:57:18 +0700

    "create route express"

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const app = express();
-const port = 8081;
+const app = express(); // app express
+const port = 8081; //port
 
+// khai báo route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/abc', (req, res) => {
+  res.send('Hello abc');
+});
 
+app.get('/hoidanit', (req, res) => {
+  res.send('<h1>Hello hoidanit</h1>');
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
