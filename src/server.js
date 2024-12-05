c2620d2f08a65a5ccee0e120f21a34a9d97b1663
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-12-05 09:23:59 +0700

    set up ENV

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+console.log('check env, ', process.env);
 const app = express(); // app express
-const port = 8081; //port
-
+const port = process.env.PORT || 8888; //port
+const hostname = process.env.HOST_NAME;
 //config template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,6 +19,6 @@ app.get('/abc', (req, res) => {
 app.get('/hoidanit', (req, res) => {
   res.render('sample.ejs');
 });
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
