0bb178bf40ae098e18f0c3ff5789907304cd4e79
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-12-05 15:45:20 +0700

    "MVC config viewengine"

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
+
 const configViewEngine = (app) => {
   //config template engine
-  console.log('>>check dirname, ', path.join('./src', 'views'));
   app.set('views', path.join('./src', 'views'));
   app.set('view engine', 'ejs');
+
   //config static files
   app.use(express.static(path.join('./src', 'public')));
 };
--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
-require('dotenv').config();
 const express = require('express');
-// create path to current directory
+const path = require('path');
+require('dotenv').config();
+
 const configViewEngine = require('./config/viewEngine');
-const webRoutes = require('./routes/web');
-const app = express(); // app express
-const port = process.env.PORT || 8888; //port
+
+const app = express();
+const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
-//config template engine
+
 configViewEngine(app);
 
-//khai báo route
-app.use('/', webRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello World new start!');
+});
+
+app.get('/abc', (req, res) => {
+  res.send('Hello World abc!');
+});
+
+app.get('/hoidanit', (req, res) => {
+  // res.send('<h1>hoidanit</h1>');
+  res.render('sample.ejs');
+});
+
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
