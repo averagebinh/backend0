7db068ccab134c41bc6adefebac1412ebd5f4948
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-12-09 16:59:06 +0700

    Navbar SSR EJS res.render()

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,15 +1,7 @@
 const connection = require('../config/database');
 
 const getHomePage = (req, res) => {
-  //process data
-  //call model
-  let users = [];
-  connection.query('SELECT * FROM Users', function (err, results, fields) {
-    users = results;
-    console.log('>>>result1: ', results); // rows returned by server
-    console.log('>>> check users ', users);
-    res.send(JSON.stringify(users));
-  });
+  return res.render('home.ejs');
 };
 
 const getABC = (req, res) => {
