2a8dbb5dfec578fd6ac92b2d0ef7c66b4b492815
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-12-09 16:42:20 +0700

    Connection Pool (createConnection)

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,8 +1,15 @@
+const connection = require('../config/database');
+
 const getHomePage = (req, res) => {
   //process data
   //call model
-
-  res.send('Hello World new start!');
+  let users = [];
+  connection.query('SELECT * FROM Users', function (err, results, fields) {
+    users = results;
+    console.log('>>>result1: ', results); // rows returned by server
+    console.log('>>> check users ', users);
+    res.send(JSON.stringify(users));
+  });
 };
 
 const getABC = (req, res) => {
