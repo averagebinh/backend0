cdb56736eb9ab2a2e8117eab9890833793b4016e
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-12-09 14:56:47 +0700

    "create connection mysql2"

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 require('dotenv').config();
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./route/web');
+// Get the client
+const mysql = require('mysql2');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -12,6 +14,26 @@ configViewEngine(app);
 
 //route
 app.use('/', webRoutes);
+
+//test connection
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit',
+});
+
+// A simple SELECT query
+connection.query('SELECT * FROM Users', (err, results, fields) => {
+  if (err) {
+    console.log('Error executing query:', err);
+  } else {
+    console.log('>>>result: ', results); // rows returned by server
+    console.log('>>>fields: ', fields); // extra metadata about results
+  }
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
