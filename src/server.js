5abda8c480c901a309f69fa8cbc4dde693e7dd3c
Nguyen Thanh Binh <averagebinh@gmail.com>
2024-12-16 14:14:34 +0700

    set up MongoDB driver

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./route/web');
 const apiRoutes = require('./route/api');
 const fileUpload = require('express-fileupload');
-
 const connection = require('./config/database');
+// mongodb driver
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
@@ -27,7 +29,22 @@ app.use('/v1/api/', apiRoutes);
 (async () => {
   try {
     // test connection
-    await connection();
+    //off mongoose
+    // await connection();
+
+    //using mongodb driver
+    // Connection URL
+    const url = process.env.DB_HOST_WITH_DRIVER;
+    const client = new MongoClient(url);
+
+    // Database Name
+    const dbName = process.env.NAME;
+
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+
     app.listen(port, hostname, () => {
       console.log(`Backend zero app listening on port ${port}`);
     });
